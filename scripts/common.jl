21ee560d0bd88996334e24c7628e955045d543df
Connor Slade <connor@connorcode.com>
2025-06-03 22:35:11 -0400

    Update angle processing scripts

--- scripts/common.jl
+++ scripts/common.jl
@@ -1,9 +1,6 @@
-using CairoMakie
 using DataStructures
-using Makie
+using ImageFiltering
 using Statistics
-
-THRESHOLD = 0.2
 
 struct Cell
 	pressure::Float32
@@ -15,41 +12,4 @@ function load_state(name)
 	(width, height) = reinterpret(UInt32, state[1:8])
 	println("[*] Loading state `$name` ($width × $height)")
 	return reshape(reinterpret(Cell, state[9:end]), (width, height))
-end
-
-function locate_blobs(state)
-	seen = Set()
-	queue = []
-	blobs = []
-
-	for x in 1:size(state, 1)
-		for y in 1:size(state, 2)
-			if !((x, y) in seen) && state[x, y].pressure > THRESHOLD
-				push!(queue, (x, y))
-				blob = []
-
-				while !isempty(queue)
-					(cx, cy) = pop!(queue)
-					if !in((cx, cy), seen) && cx > 0 && cx <= size(state, 1) && cy > 0 && cy <= size(state, 2) && state[cx, cy].pressure > THRESHOLD
-						push!(blob, (cx, cy))
-						push!(seen, (cx, cy))
-
-						for delta in [(-1, 0), (1, 0), (0, -1), (0, 1)] 
-							dx, dy = delta
-							nx, ny = cx + dx, cy + dy
-							if nx > 0 && nx <= size(state, 1) && ny > 0 && ny <= size(state, 2) && state[nx, ny].pressure > THRESHOLD && !in((nx, ny), seen)
-								push!(queue, (nx, ny))
-							end
-						end
-					end
-				end
-
-				if !isempty(blob)
-					push!(blobs, blob)
-				end
-			end
-		end
-	end
-
-	return map(blob -> (mean(map(coord -> coord[1], blob)), mean(map(coord -> coord[2], blob))), blobs)
 end
--- scripts/visualize.jl
+++ scripts/visualize.jl
@@ -1,15 +1,79 @@
-state = load_state("1748997635.bin")
-center_x, center_y = size(state, 1) / 2, size(state, 2) / 2
+using CairoMakie
+using Makie
 
-blobs = locate_blobs(state)
-println("[*] Found $(length(blobs)) blobs")
-angles = map(blob -> atan(blob[2] - center_y, blob[1] - center_x), blobs)
+include("common.jl")
 
-f, ax, hm = heatmap(map(x -> x.pressure, state))
-hidedecorations!(ax)
+PRESSURE_THRESHOLD = 0.25
+AREA_THRESHOLD = 200
 
-for (x, y) in blobs
-	scatter!(ax, [x], [y], color=:red, markersize=10)
+function locate_blobs(state, pressure_clip, area_clip)
+	pressures = imfilter(map(cell -> cell.pressure, state), Kernel.gaussian(6))
+	in_bounds = (x, y) -> x > 0 && x <= size(state, 1) && y > 0 && y <= size(state, 2)
+
+	seen = Set()
+	queue = []
+	blobs = []
+
+	for x in 1:size(state, 1), y in 1:size(state, 2)
+		threshold = (x, y) -> pressures[x, y] > pressure_clip
+
+		if !in((x, y), seen) && threshold(x, y)
+			push!(queue, (x, y))
+			blob = []
+
+			while !isempty(queue)
+				(cx, cy) = pop!(queue)
+				if !in((cx, cy), seen) && in_bounds(cx, cy) && threshold(cx, cy)
+					push!(blob, (cx, cy))
+					push!(seen, (cx, cy))
+
+					for delta in [(-1, 0), (1, 0), (0, -1), (0, 1)]
+						dx, dy = delta
+						nx, ny = cx + dx, cy + dy
+						if in_bounds(nx, ny) && threshold(nx, ny) && !in((nx, ny), seen)
+							push!(queue, (nx, ny))
+						end
+					end
+				end
+			end
+
+			if !isempty(blob)
+				push!(blobs, blob)
+			end
+		end
+	end
+
+	blobs = [(mean(x for (x, _) in blob), mean(y for (_, y) in blob)) for blob in blobs if length(blob) >= area_clip]
+	return pressures, blobs
 end
 
-f
+states = readdir("states")
+states = filter(state -> endswith(state, ".bin"), states)
+states = sort(states, by = x -> parse(Int, splitext(basename(x))[1]))
+
+n = ceil(Int, sqrt(length(states)))
+f = Figure(size = (800, 800))
+
+data = []
+for (i, state_file) in enumerate(states)
+	state = load_state(state_file)
+	pressure, blobs = locate_blobs(state, PRESSURE_THRESHOLD, AREA_THRESHOLD)
+	center_x, center_y = size(state, 1) / 2, size(state, 2) / 2
+	angles = map(blob -> atan(blob[2] - center_y, blob[1] - center_x), blobs)
+	append!(data, [(i-1, angle) for angle in angles])
+
+	fig = f[div(i-1, n), mod(i-1, n)]
+	ax = Axis(fig, aspect=DataAspect(), title="θ=$(map(θ -> round(θ, digits=2), angles))")
+	heatmap!(ax, map(x -> x > PRESSURE_THRESHOLD, pressure))
+	hidedecorations!(ax)
+
+	for (x, y) in blobs
+		scatter!(ax, [x], [y], color=:red)
+	end
+end
+
+for (h, angle) in data
+	println("$(2*h),$angle")
+end
+
+display(f)
