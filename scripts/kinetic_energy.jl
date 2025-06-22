c71892e4fe79f82a224c16138aa8f8a7fd6596e9
Connor Slade <connor@connorcode.com>
2025-06-22 13:09:07 -0400

    Add plot lables

--- scripts/kinetic_energy.jl
+++ scripts/kinetic_energy.jl
@@ -10,13 +10,13 @@ energies = []
 
 for path in states
     state = load_state(path)
-    energy = reduce(+, map(x -> x.pressure * (x.velocity[1]^2 + x.velocity[2]^2), state))
+    energy = reduce(+, map(x -> sqrt(x.velocity[1]^2 + x.velocity[2]^2), state))
     
-    push!(energies, energy / 2)
+    push!(energies, energy)
 end
 
 f = Figure()
-ax = Axis(f[1, 1], limits = (nothing, (0, 50)))
+ax = Axis(f[1, 1], title = "Velocity over Time", xlabel = "Ticks", ylabel = "Velocity")
 lines!(ax, 1:length(energies), energies)
 
 f
